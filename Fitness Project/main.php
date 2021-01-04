b243d52e8c0c06dcfccad834a4cb35fbd06d924c
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2021-01-04 10:25:05 -0600

    Update main.php
    Updated code to remove unnecessary values.

--- Fitness Project/main.php
+++ Fitness Project/main.php
@@ -1,10 +1,10 @@
-<html><head><title>CSCI 466 - Group 2 - Second Stringers</title></head><body>
-	<h1>CSCI 466 Group Project - Group 2 - Second Stringers</h1>
+<html><head><title>Fitness Project</title></head><body>
+	<h1>Fitness Project</h1>
 	<h4>Note: All units will be shown as grams, unless otherwise specified</h4>
 <?php
   define("MEASUREMENT", ["lb","oz","mg","g","kg","c","p","ml","l","dl"]);
   try{
-		$dsn = "mysql:host=courses;dbname=z1867886";
+		$dsn = "mysql:host=DB_HOST;dbname=DB_NAME";
 		$pdo = new PDO($dsn,$user,$password);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	}
